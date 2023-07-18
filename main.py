5b8c069a6ba5dff8c33dcfbe018ccf459aa5b93d
Loïc <loicdelattre38@gmail.com>
2023-07-18 16:26:15 +0200

    trying to get info from unit page start

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from bs4 import BeautifulSoup
 homePageUrl = "https://empireearth.fandom.com/wiki/Category:Empires:_Dawn_of_the_Modern_World_Units"
 baseUrl = "https://empireearth.fandom.com/"
 
-page = requests.get(homePageUrl, verify = False)
+pageHome = requests.get(homePageUrl, verify = False)
 
-htmlData = BeautifulSoup(page.content, "html.parser")
+htmlDataHome = BeautifulSoup(pageHome.content, "html.parser")
 
-unitsSpace = htmlData.find_all("div", class_ = "category-page__member-left")
+unitsSpace = htmlDataHome.find_all("div", class_ = "category-page__member-left")
 
 unitsLinks = []
 
 for item in unitsSpace:
     attribute = item.find_all("a")[0] #outpus a 1 element list hence [0]
-    unitsLinks.append(attribute["href"])
+    linkUnit = baseUrl + attribute["href"]
 
-print(unitsLinks)
+    pageUnit = requests.get(linkUnit, verify = False)
+    htmlDataUnit = BeautifulSoup(pageUnit.content, "html.parser")
+    unitData = htmlDataUnit.find_all("div", class_ = "mw-parser-output")[0]
+    print(unitData)
+
+    break 
+    for area in unitData:
+        summary = area.find_all("p")
+        print(area)
+        break
+    break
