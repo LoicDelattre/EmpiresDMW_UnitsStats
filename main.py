c3a8c02f66e9584a93f21d39583d0a82743e56ad
Loïc <loicdelattre38@gmail.com>
2023-07-18 16:04:50 +0200

    returns a list of units' links

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 homePageUrl = "https://empireearth.fandom.com/wiki/Category:Empires:_Dawn_of_the_Modern_World_Units"
+baseUrl = "https://empireearth.fandom.com/"
 
 page = requests.get(homePageUrl, verify = False)
 
-print(page.text)
+htmlData = BeautifulSoup(page.content, "html.parser")
 
+unitsSpace = htmlData.find_all("div", class_ = "category-page__member-left")
+
+unitsLinks = []
+
+for item in unitsSpace:
+    attribute = item.find_all("a")[0] #outpus a 1 element list hence [0]
+    unitsLinks.append(attribute["href"])
+
+print(unitsLinks)
